38aa3feb85b39a27e764a6bc7f1f77e2a9f4e3e9
asylbek09 <asylbek09@gmail.com>
2021-04-12 13:55:27 -0400

    binary to decimal conversion

--- src/BinaryToDecimal.java
+++ src/BinaryToDecimal.java
@@ -1,3 +1,16 @@
 public class BinaryToDecimal {
+    public static void main(String[] args) {
+        String binary = "10110";
+        System.out.println(decimal(binary));
+    }
 
+    static int decimal(String num) {
+        int result = 0;
+        int end = num.length() - 1;
+        for (int i = 0; i <= end; i++) {
+            int current = Character.getNumericValue(num.charAt(end - i));
+            result += Math.pow(2, i) * current;
+        }
+        return result;
+    }
 }
